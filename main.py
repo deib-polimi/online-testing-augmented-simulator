156d2f310cd2fc712028264891155ddd77b713d3
dragonbanana <davideyi.hu@polimi.it>
2024-02-12 18:50:20 +0100

    fix: adjusted simulator path

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ import torchvision.transforms as t
 # Take arguments
 host = "127.0.0.1"
 port = 4567
-simulator_exe_path = "/media/banana/5E32C4AD32C48C09/Users/DAVID/Documents/self-driving-car-sim-new/Builds/udacity.x86_64"
+simulator_exe_path = "./simulator/udacity.x86_64"
 checkpoint = "lake_sunny_day_60_0.ckpt"
 run_name = "fluffy_pony"
 
