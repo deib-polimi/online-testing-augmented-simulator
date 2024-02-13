32ef307411d8dd24123da40af5f97396de2f749f
dragonbanana <davideyi.hu@polimi.it>
2024-02-13 16:54:48 +0100

    feat: add ip2p

---
+++ augment/dm/ip2p.py
@@ -0,0 +1,27 @@
+# InstructPix2Pix
+import torch
+import PIL
+import requests
+import torch
+from diffusers import StableDiffusionInstructPix2PixPipeline, EulerAncestralDiscreteScheduler
+from diffusers.utils import load_image
+
+
+class InstructPix2Pix:
+
+    def __init__(self, prompt: str, guidance: float):
+        self.pipe = StableDiffusionInstructPix2PixPipeline.from_pretrained("timbrooks/instruct-pix2pix",
+                                                                           torch_dtype=torch.float16,
+                                                                           safety_checker=None)
+        # pipe.to(device)
+        self.pipe.scheduler = EulerAncestralDiscreteScheduler.from_config(self.pipe.scheduler.config)
+        self.prompt = prompt
+        self.guidance = guidance
+
+    def forward(self, x):
+        transformed_images = self.pipe(prompt=self.prompt, image=x, num_images_per_prompt=1,
+                                       num_inference_steps=50, guidance_scale=10,
+                                       image_guidance_scale=self.guidance).images
+
+if __name__ == '__main__':
+    x = InstructPix2Pix("make it blue", 2.0)
