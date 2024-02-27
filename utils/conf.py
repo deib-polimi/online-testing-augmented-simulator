163c3e1b6da3836259e6a6bcad8fb0dbb5507ce2
dragonbanana <davideyi.hu@polimi.it>
2024-02-27 01:09:54 +0100

    refactor: switch to cuda:1

--- utils/conf.py
+++ utils/conf.py
@@ -1,6 +1,6 @@
 import pathlib
 
-DEFAULT_DEVICE = "cuda:0"
+DEFAULT_DEVICE = "cuda:1"
 PROJECT_DIR = pathlib.Path(__file__).parent.parent.absolute()
 
 RESULT_DIR = pathlib.Path("/media/banana/data/results/online-testing")
