a7253caaf5422050abf0b0dd6405f338927f35e4
Davide Yi Xian Hu <davideyi.hu@polimi.it>
2024-06-19 16:17:59 +0200

    feat: adapted sd inpainting

--- utils/image_preprocess.py
+++ utils/image_preprocess.py
@@ -8,7 +8,7 @@ import torchvision
 def to_pytorch_tensor(x):
     if isinstance(x, str) or isinstance(x, pathlib.Path) or isinstance(x, os.PathLike):
         return torchvision.transforms.ToTensor()(PIL.Image.open(x))
-    elif isinstance(x, PIL.Image.Image) or isinstance(x, np.array):
+    elif isinstance(x, PIL.Image.Image) or isinstance(x, np.ndarray):
         return torchvision.transforms.ToTensor()(x)
     else:
         return x.squeeze(0) if len(x.shape) == 4 else x
